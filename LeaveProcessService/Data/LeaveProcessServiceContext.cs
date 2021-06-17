5f11c249b338132784a67d663fa1c587982f3a32
Chiến Nguyễn <33652672+ChienNguyensrdn@users.noreply.github.com>
2021-06-17 10:58:01 +0700

    change context

--- LeaveProcessService/Data/LeaveProcessServiceContext.cs
+++ LeaveProcessService/Data/LeaveProcessServiceContext.cs
@@ -11,11 +11,15 @@ namespace LeaveProcessService.Data
         {
         }
         public  DbSet<LeaveProcessService.Entities.LeaveType> LeaveTypes { get; set; }
-        public DbSet<LeaveProcessService.Entities.LeaveSymbol > LeaveSymbols   { get; set; }
+        public  DbSet<LeaveProcessService.Entities.LeaveSymbol > LeaveSymbols   { get; set; }
+        public  DbSet<LeaveProcessService.Entities.LeaveShift> LeaveShifts { get; set; }
+        public  DbSet<LeaveProcessService.Entities.OtherListType> OtherListTypes { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<LeaveProcessService.Entities.LeaveType>().ToTable("AT_FML", "SNP_CR");
+            modelBuilder.Entity<LeaveProcessService.Entities.LeaveShift>().ToTable("OT_OTHER_LIST", "SNP_CR");
             modelBuilder.Entity<LeaveProcessService.Entities.LeaveSymbol>().ToTable("AT_TIME_MANUAL", "SNP_CR");
+            modelBuilder.Entity<LeaveProcessService.Entities.OtherListType>().ToTable("OT_OTHER_LIST_TYPE", "SNP_CR");
         }
     }
 }
