0ca517b47dd3c378bdea963e25545603b90676e5
Chiến Nguyễn <33652672+ChienNguyensrdn@users.noreply.github.com>
2021-06-17 10:35:17 +0700

    add file

--- LeaveProcessService/Entities/OtherListType.cs
+++ LeaveProcessService/Entities/OtherListType.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
 namespace LeaveProcessService.Entities
 {
+    [Table("SNP_CR.OT_OTHER_LIST_TYPE")]
     public class OtherListType
     {
-        public OtherListType()
-        {
-        }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public string CODE { get; set; }
+        public string NAME{ get; set; }
     }
 }
