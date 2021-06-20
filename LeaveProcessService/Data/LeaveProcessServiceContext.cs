25f14ff9c5b1d775507aa4f82eeb47caa2a5fdd3
Chiến Nguyễn <33652672+ChienNguyensrdn@users.noreply.github.com>
2021-06-20 21:10:32 +0700

    chinh sua

--- LeaveProcessService/Data/LeaveProcessServiceContext.cs
+++ LeaveProcessService/Data/LeaveProcessServiceContext.cs
@@ -1,7 +1,7 @@
 ﻿using Oracle.EntityFrameworkCore;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
-
+using Microsoft.EntityFrameworkCore.Metadata;
 namespace LeaveProcessService.Data
 {
     public class LeaveProcessServiceContext:DbContext 
