f3c8f0b6714a6ef86df3f4b86f23648d1d51a521
chiennv <chiennv@tinhvan.com>
2021-06-21 17:51:21 +0700

    add method post validate

--- LeaveProcessService/Controllers/LeaveProcessController.cs
+++ LeaveProcessService/Controllers/LeaveProcessController.cs
@@ -11,6 +11,8 @@ using System.Data;
 using Oracle.ManagedDataAccess.Client;
 using Microsoft.Extensions.Configuration;
 using LeaveProcessService.DataAcessHelper;
+using System.Net;
+
 namespace LeaveProcessService.Controllers
 {
     [Route("api/[controller]")]
@@ -26,7 +28,54 @@ namespace LeaveProcessService.Controllers
             _configuration = configuration;
         }
         #region Leave process
-        [Route("")]
+        [Route("validate-register-leave")]
+        [HttpPost]
+        public async Task<ActionResult<ResponseResult>> ValidateRgisterLeave(ValidateLeavesheet leavesheet)
+        {
+            String message = string.Empty;
+            int reponseStatus = 200;
+            //string clientContentType; string clientAccept;
+            try
+            {
+                //clientContentType = Request.Headers.ContentType.ToString();
+                //clientAccept = Request.Headers.ToString();
+               
+                OracleCommand command = new OracleCommand();
+                command.CommandText = "PKG_ATTENDANCE_BUSINESS.VALIDATE_LEAVE";
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add(new OracleParameter("P_EMP_ID", OracleDbType.Decimal, leavesheet.employeeId, ParameterDirection.Input));
+                command.Parameters.Add(new OracleParameter("P_FROMDATE", OracleDbType.NVarchar2, 8, leavesheet.leaveFrom, ParameterDirection.Input));
+                command.Parameters.Add(new OracleParameter("P_TODATE", OracleDbType.NVarchar2, 8, leavesheet.leaveTo, ParameterDirection.Input));
+                command.Parameters.Add(new OracleParameter("P_LEAVETYPE", OracleDbType.Decimal, 10, leavesheet.leaveType, ParameterDirection.Input));
+                command.Parameters.Add(new OracleParameter("P_LEAVETIME", OracleDbType.Decimal, 10, leavesheet.leaveTime, ParameterDirection.Input));
+                command.Parameters.Add(new OracleParameter("P_LEAVE_ID", OracleDbType.Decimal, 10, leavesheet.Id, ParameterDirection.Input));
+                command.Parameters.Add(new OracleParameter("P_MESSAGE", OracleDbType.NVarchar2, 500, message, ParameterDirection.Output));
+                command.Parameters.Add(new OracleParameter("P_RESPONSESTATUS", OracleDbType.Int16, 10, reponseStatus, ParameterDirection.Output));
+                _oracleDBManager = new OracleDBManager(command, _configuration.GetConnectionString("DbConnect").ToString());
+                await _oracleDBManager.ExecuteNonQueryAsync();
+                ResponseResult responseResult = new ResponseResult();
+                responseResult.Responsestatus = int.Parse(command.Parameters["P_RESPONSESTATUS"].Value?.ToString());
+                if (responseResult.Responsestatus > 0)
+                {
+                    responseResult.Responsestatus = (int)HttpStatusCode.OK;
+                }
+                else
+                {
+                    responseResult.Responsestatus = (int)HttpStatusCode.NotFound;
+                }
+                responseResult.Message = command.Parameters["P_MESSAGE"].Value.ToString(); ;
+                return responseResult;
+            }
+            catch (Exception ex)
+            {
+                ResponseResult responseResult = new ResponseResult();
+                responseResult.Message = ex.ToString();
+                responseResult.Responsestatus = (int)HttpStatusCode.BadRequest;
+                return responseResult;
+            }
+           
+        }
+      
         #endregion Leave process
 
         #region List 
